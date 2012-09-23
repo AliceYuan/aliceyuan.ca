fda24d1f36ea14059b960b68392987887cfe7283
AliceYuan <alice.y@myplanetdigital.com>
2012-09-22 21:30:48 -0400

    added more icons, and trimmed length for recent posts in front page

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -128,34 +128,34 @@ class SiteController extends Controller
 		Yii::app()->user->logout();
 		$this->redirect(Yii::app()->homeUrl);
 	}
-
-  public function abbreviate($input,$width) {
-    if(empty($input)) return $input ;
-
-    if (strlen($input) <= $width) {
-        return $input;
-    }
-
-    $output = substr($input,0,$width);
-
-    //normals words are seldom more than 30 chars
-    $pos = 0 ;
-    $found = false ;
-
-    for($i = $width ; $i >= 0 ; $i--) {
-         if(ctype_space($output[$i])) {
-            $found = true ;
-            break ;
-         }
-         $pos++ ;
-    }
-
-    if($found && ($pos > 0)) {
-        $output = substr($output,0,($width-$pos));
-        $output = rtrim($output) ;
-    }
-
-    $output .= '...' ;
-    return $output;
-  }
+//
+//   public function abbreviate($input,$width) {
+//     if(empty($input)) return $input ;
+//
+//     if (strlen($input) <= $width) {
+//         return $input;
+//     }
+//
+//     $output = substr($input,0,$width);
+//
+//     //normals words are seldom more than 30 chars
+//     $pos = 0 ;
+//     $found = false ;
+//
+//     for($i = $width ; $i >= 0 ; $i--) {
+//          if(ctype_space($output[$i])) {
+//             $found = true ;
+//             break ;
+//          }
+//          $pos++ ;
+//     }
+//
+//     if($found && ($pos > 0)) {
+//         $output = substr($output,0,($width-$pos));
+//         $output = rtrim($output) ;
+//     }
+//
+//     $output .= '...' ;
+//     return $output;
+//   }
 }
--- protected/views/site/_recent.php
+++ protected/views/site/_recent.php
@@ -1,10 +1,19 @@
 <?php
-  // $shortened_content = strip_tags ( $data->content , '<p><span>');
-  // $shortened_content = abbreviate($shortened_content, 150);
+  $shortened_content = strip_tags ( $data->content , '<p><span>');
+  $input = $shortened_content;
+  $length = 350;
+  if (!(strlen($input) <= $length)) {
+    $last_space = strrpos(substr($input, 0, $length), ' ');
+    $trimmed_text = substr($input, 0, $last_space);
+
+    //add ellipses (...)
+    $trimmed_text .= '...';
+    $shortened_content = $trimmed_text;
+  }
 ?>
 <li class="post-<?php echo $data->category?>" id="post-<?php echo date('mdy',$data->create_time)?>">
 
-<a href="<?php echo $data->url;?>">
+<a href="<?php echo $data->url;?>" class="clearfix">
     <div class="date">
       <p><span class="M"><?php echo date('M',$data->create_time); ?></span>
       <span class="D"><?php echo date('j',$data->create_time); ?></span>
@@ -15,7 +24,7 @@
       <p class="shortened-desc">
       <?php
         $this->beginWidget('CMarkdown', array('purifyOutput'=>true));
-        echo strip_tags ( $data->content , '<p><span>');
+        echo $shortened_content;
         $this->endWidget();
       ?>
     </div>
