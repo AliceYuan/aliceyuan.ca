b9f5a44a30089f1a39c64d5f31dacae965f7049b
AliceYuan <alice.y@myplanetdigital.com>
2012-09-24 14:32:10 -0400

    removed files

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -20,16 +20,11 @@ $this->pageTitle=Yii::app()->name . ' - About';
        <p>
       <span class="hello">Hello!</span> The <a href="//twitter.com/Names_Alice"><span class="ss-twitter"></span>Names_Alice</a> and I'm a student studying at the University of Waterloo pursuing an undergraduate degree in Software Engineering. I am passionate about writing beautiful code that create applications with aesthetic and functional interfaces.
       </p>
+      <p>
+      Call me young, but I'm not ill-experienced. I have interned as a web developer at  <a href="//myplanetdigital.com" class="mpd">Myplanet Digital</a> this past summer (2012) and have had the privilege of attending the <a href="http://googleblog.blogspot.ca/2011/08/fast-tracking-young-web-developers-at.html?"> Computer Science Summer Institute at Google </a>  the summer prior (2011).
 
-<!--       <p> -->
-<!-- For most of my childhood, I fantasied about a career immersed in visual arts. But one little word flipped that dream upside down: programming. -->
-<!-- Perhaps one would consider me a newbie in the world of programming; but I dream high, I work hard and I learn fast. -->
-<!-- </p> -->
-<p>
-Call me young, but I'm not ill-experienced. I have interned as a web developer at  <a href="//myplanetdigital.com" class="mpd">Myplanet Digital</a> this past summer (2012) and have had the privilege of attending the <a href="http://googleblog.blogspot.ca/2011/08/fast-tracking-young-web-developers-at.html?"> Computer Science Summer Institute at Google </a>  the summer prior (2011).
-
-I have experience with both back-end and front-end development and have also experimented with web design in my spare time. I'm still at that “self-discovery phase” and I love that I'm learning new things everyday.</p>
-<p>
+      I have experience with both back-end and front-end development and have also experimented with web design in my spare time. I'm still at that “self-discovery phase” and I love that I'm learning new things everyday.</p>
+      <p>
       </p>
       <p>
       My other hobbies include: reading, drawing, photography, and going on adventures.
