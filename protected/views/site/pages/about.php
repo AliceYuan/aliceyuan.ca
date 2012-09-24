1e1b07cc3d6cbf2c7515875779c0077308c1a701
AliceYuan <alice.y@myplanetdigital.com>
2012-09-24 11:47:13 -0400

    grammar fixes on about page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -18,8 +18,7 @@ $this->pageTitle=Yii::app()->name . ' - About';
   <div class="page-content">
     <div class="about-me">
        <p>
-      <span class="hello">Hello!</span> The <a href="//twitter.com/Names_Alice"><span class="ss-twitter"></span>Names_Alice</a> and I'm a student studying at the University of Waterloo pursuing an undergraduate degree in software engineering. I am passionate about writing beautiful code that create applications with aesthetic and functional interfaces.
-
+      <span class="hello">Hello!</span> The <a href="//twitter.com/Names_Alice"><span class="ss-twitter"></span>Names_Alice</a> and I'm a student studying at the University of Waterloo pursuing an undergraduate degree in Software Engineering. I am passionate about writing beautiful code that create applications with aesthetic and functional interfaces.
       </p>
 
 <!--       <p> -->
@@ -27,13 +26,13 @@ $this->pageTitle=Yii::app()->name . ' - About';
 <!-- Perhaps one would consider me a newbie in the world of programming; but I dream high, I work hard and I learn fast. -->
 <!-- </p> -->
 <p>
-Call me young, but I'm not ill-experienced. I have interned as a web developer at  <a href="//myplanetdigital.com" class="mpd">Myplanet Digital</a> this past summer (2012) and have had the privilege of attending the <a href="http://googleblog.blogspot.ca/2011/08/fast-tracking-young-web-developers-at.html?"> computer science summer institute at Google </a>  the summer prior (2011).
+Call me young, but I'm not ill-experienced. I have interned as a web developer at  <a href="//myplanetdigital.com" class="mpd">Myplanet Digital</a> this past summer (2012) and have had the privilege of attending the <a href="http://googleblog.blogspot.ca/2011/08/fast-tracking-young-web-developers-at.html?"> Computer Science Summer Institute at Google </a>  the summer prior (2011).
 
-I have experience with both back end and front end development and have also experimented with web-design in my spare time. I'm still at that “self-discovery phase” and I love that I'm learning new things everyday.</p>
+I have experience with both back-end and front-end development and have also experimented with web design in my spare time. I'm still at that “self-discovery phase” and I love that I'm learning new things everyday.</p>
 <p>
       </p>
       <p>
-      My other hobbies include: reading, drawing, photography and going on adventures.
+      My other hobbies include: reading, drawing, photography, and going on adventures.
       </p>
       <p>
       Want to talk? I'm always up for a chat!
